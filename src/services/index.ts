77a0a4371492a91001fda9ef122ce057ae76844f
Angeles Rondon <angelesemilianaa@gmail.com>
2023-07-31 10:54:20 -0500

    Add modules to services main file

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,1 +1,5 @@
 export * from './user.services';
+export * from './account.services';
+export * from './currency.services';
+export * from './transaction.services';
+export * from './exchange.services';

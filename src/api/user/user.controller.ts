3c4f60d1750fb95e9975527de5c811216bd4d389
Angeles Rondon <angelesemilianaa@gmail.com>
2023-08-03 09:40:07 -0500

    Delete schema to validate user data

--- src/api/user/user.controller.ts
+++ src/api/user/user.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { services } from '../../services';
-import { SignUp, userSchema } from '../../models';
+import { SignUp } from '../../models';
 import { UserService } from '../../services/user.services';
 
 export class UserController {
@@ -13,11 +13,6 @@ export class UserController {
   public signUp = async (req: Request, res: Response) => {
     const { last_name, first_name, email, password } = req.body;
     const signUp = new SignUp(last_name, first_name, email, password);
-
-    const validateUserData = userSchema.safeParse(signUp);
-    if (!validateUserData.success) {
-      throw validateUserData.error;
-    }
 
     const newUser = await this.userService.createUser(signUp);
     res.status(200).send(newUser);

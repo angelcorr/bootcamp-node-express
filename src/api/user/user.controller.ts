b35ed860a79c426508a39c371a816d1c1315b5d6
Angeles Rondon <angelesemilianaa@gmail.com>
2023-08-03 10:29:04 -0500

    Change class with typo sign up model
    * Modify sign up type implementation

--- src/api/user/user.controller.ts
+++ src/api/user/user.controller.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from 'express';
 import { services } from '../../services';
-import { SignUp } from '../../models';
 import { UserService } from '../../services/user.services';
 
 export class UserController {
@@ -11,8 +10,8 @@ export class UserController {
   }
 
   public signUp = async (req: Request, res: Response) => {
-    const { last_name, first_name, email, password } = req.body;
-    const signUp = new SignUp(last_name, first_name, email, password);
+    const { lastName, firstName, email, password } = req.body;
+    const signUp = { lastName, firstName, email, password };
 
     const newUser = await this.userService.createUser(signUp);
     res.status(200).send(newUser);
--- src/models/signUp.model.ts
+++ src/models/signUp.model.ts
@@ -1,22 +1,15 @@
 import { z } from 'zod';
 
-export class SignUp {
-  public last_name: string;
-  public first_name: string;
-  public email: string;
-  public password: string;
-
-  constructor(last_name: string, first_name: string, email: string, password: string) {
-    this.last_name = last_name;
-    this.first_name = first_name;
-    this.email = email;
-    this.password = password;
-  }
-}
+export type SignUp = {
+  lastName: string;
+  firstName: string;
+  email: string;
+  password: string;
+};
 
 export const userSchema = z.object({
-  first_name: z.string(),
-  last_name: z.string(),
+  firstName: z.string(),
+  lastName: z.string(),
   email: z.string().email(),
   password: z.string(),
 });

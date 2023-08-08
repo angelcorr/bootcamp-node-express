b7b3871fa04c76a65f725995b5adb83a2cc9068a
Angeles Rondon <angelesemilianaa@gmail.com>
2023-08-08 09:15:27 -0500

    Create auth middleware to validate token
    * Create new passport jsonwebtoken strategy

--- src/middlewares/passport.ts
+++ src/middlewares/passport.ts
@@ -1,10 +1,11 @@
 import passport from 'passport';
-import passportLocal from 'passport-local';
+import { Strategy as LocalStrategy } from 'passport-local';
+import { ExtractJwt, Strategy as JwtStrategy } from 'passport-jwt';
 import bcrypt from 'bcrypt';
 import { services } from '../services';
 import UnauthorizedError from '../customErrors/unauthorizedError';
+import constants from '../constants';
 
-const LocalStrategy = passportLocal.Strategy;
 const localStrategy = new LocalStrategy(
   { usernameField: 'email', passwordField: 'password' },
   async (email: string, password: string, done) => {
@@ -24,6 +25,20 @@ const localStrategy = new LocalStrategy(
   },
 );
 
+const jwtStrategyOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: constants.JWT_SECRET,
+};
+const jwtStrategy = new JwtStrategy(jwtStrategyOptions, (jwtPayload, done) => {
+  const user = services.userService.get(jwtPayload.email);
+  if (!user) {
+    done(new UnauthorizedError('The email or password are incorrect'));
+  } else {
+    done(null, user);
+  }
+});
+
 passport.use(localStrategy);
+passport.use(jwtStrategy);
 
 export default passport;

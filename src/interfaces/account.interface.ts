23ac7fd9ace351ce0d1de0cec654678b8e8718c1
Angeles Rondon <angelesemilianaa@gmail.com>
2023-08-11 10:51:12 -0500

    Delete models that should be Data Transfer Objects

--- src/interfaces/account.interface.ts
+++ src/interfaces/account.interface.ts
@@ -1,4 +1,5 @@
-import { Account, NewAccount } from '../models';
+import { NewAccount } from '../dataTransferObjects/newAccount.object';
+import { Account } from '../models';
 import IService from './service.interface';
 
 export default interface IAccount extends IService<NewAccount, Account> {

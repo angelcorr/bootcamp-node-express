e31c6882172e57af30e573c87bd99d244fa1fe4e
Angeles Rondon <angelesemilianaa@gmail.com>
2023-08-11 09:22:33 -0500

    Add types to the IAccount interface

--- src/interfaces/account.interface.ts
+++ src/interfaces/account.interface.ts
@@ -1,5 +1,5 @@
 import IService from './service.interface';
 
-export default interface IAccount<M1, M2> extends IService<M1, M2> {
-  getList(identifier: string): M2[];
+export default interface IAccount<NewAccount, Account> extends IService<NewAccount, Account> {
+  getList(identifier: string): Account[];
 }

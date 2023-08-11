c80f596e5840e54db5a9e5926c61a0c543bd8d23
Angeles Rondon <angelesemilianaa@gmail.com>
2023-08-11 11:03:10 -0500

    Delete unnecessary type on IAccount

--- src/interfaces/account.interface.ts
+++ src/interfaces/account.interface.ts
@@ -1,5 +1,6 @@
+import { Account, NewAccount } from '../models';
 import IService from './service.interface';
 
-export default interface IAccount<NewAccount, Account> extends IService<NewAccount, Account> {
+export default interface IAccount extends IService<NewAccount, Account> {
   getList(identifier: string): Account[];
 }

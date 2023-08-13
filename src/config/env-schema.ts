880ccf88b82a1e9b861dcfdad697c6ae9b7197d9
Angeles Rondon <angelesemilianaa@gmail.com>
2023-08-13 10:57:50 -0500

    Add missing JWT secret to dotenv file
    * Add unnecessary DS_Store file to gitignore.

--- src/config/env-schema.ts
+++ src/config/env-schema.ts
@@ -4,6 +4,7 @@ export const envVarsSchema = z.object({
   NODE_ENV: z.string(),
   DEBUG: z.preprocess((stringBool) => Boolean(stringBool === 'true'), z.boolean()),
   PORT: z.preprocess((port) => parseInt(port as string, 10), z.number().positive().max(10000)),
+  JWT_SECRET: z.string(),
 });
 
 export type envVarsSchemaType = z.infer<typeof envVarsSchema>;
--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,7 @@
+import env from './config';
+
 export default {
   SALTED_ROUNDS: 10,
   DEFAULT_CAPITAL_AMOUNT: 0,
-  JWT_SECRET: process.env.JWT_SECRET || 'super-secret-key',
+  JWT_SECRET: env.JWT_SECRET || 'super-secret-key',
 };

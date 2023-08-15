b915b742027945bf9d6e9be43ee0f0ccb40c009f
Angeles Rondon <angelesemilianaa@gmail.com>
2023-08-15 11:14:00 -0500

    Change currencyId number type to string
    * This data change in order to be consistent with the id data type.

--- src/models/exchange.model.ts
+++ src/models/exchange.model.ts
@@ -1,9 +1,9 @@
 export class Exchange {
-  private currencyId: number;
+  private currencyId: string;
   private date: Date;
   private rate: number;
 
-  constructor(currencyId: number, date: Date, rate: number) {
+  constructor(currencyId: string, date: Date, rate: number) {
     this.currencyId = currencyId;
     this.date = date;
     this.rate = rate;

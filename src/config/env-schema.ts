3670c53d4157a7a9f977ed29692418065a7941d9
Angeles Rondon <angelesemilianaa@gmail.com>
2023-08-17 15:53:05 -0500

    Add environment variables to the api container
    * This is to have these the variables available in the api

--- src/config/env-schema.ts
+++ src/config/env-schema.ts
@@ -6,6 +6,10 @@ export const envVarsSchema = z.object({
   PORT: z.preprocess((port) => parseInt(port as string, 10), z.number().positive().max(10000)),
   JWT_SECRET: z.string(),
   ACCESS_KEY: z.string(),
+  POSTGRES_USER: z.string(),
+  POSTGRES_PASSWORD: z.string(),
+  POSTGRES_DB: z.string(),
+  POSTGRES_HOST: z.string(),
 });
 
 export type envVarsSchemaType = z.infer<typeof envVarsSchema>;

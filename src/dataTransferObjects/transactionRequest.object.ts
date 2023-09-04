8afc785241917580682e81799e9bdfd7b3a9ef13
Angeles Rondon <angelesemilianaa@gmail.com>
2023-09-04 13:43:28 -0500

    Delete userId attribute from zod validation object
    * This attribute is get by the token given by the user, is not given
    by the user via query params.

--- src/dataTransferObjects/transactionRequest.object.ts
+++ src/dataTransferObjects/transactionRequest.object.ts
@@ -15,7 +15,6 @@ export const transactionRequestSchema = z
   .object({
     page: z.coerce.number().positive(),
     pageSize: z.coerce.number().positive(),
-    userId: z.string().uuid(),
     dateFrom: z.string().datetime(),
     dateTo: z.string().datetime(),
     sortBy: z.enum(['id', 'accountId', 'time', 'description']),

d5b5a23223a3a701c40997a210f30e2aaa8c6780
Angeles Rondon <angelesemilianaa@gmail.com>
2023-09-15 09:10:46 -0500

    Create GetTransactionsArgs type
    * This is to handle the arguments send by the user when they query for
    a user data by its id.
    * Refactor user function to be an expression.

--- src/graphql/types.ts
+++ src/graphql/types.ts
@@ -22,6 +22,10 @@ export type CreateTransactionArgs = {
   description: string;
 };
 
+export type GetUserArgs = {
+  id: string;
+};
+
 export type Context = {
   transactionService: TransactionService;
   userService: UserService;
